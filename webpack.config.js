cbc2865e3f913869e4a168c604979b3b46d83387
Douglas Freitas <douglasamf14@gmail.com>
2024-11-16 16:36:53 -0300

    Subindo a estrutura do projeto, já configurada

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    devServer: {
+        static: {
+          directory: path.resolve(__dirname, 'dist')
+        },
+        compress: true,
+        port: 8080
+    },
+    entry: {
+        index: "./app/src/index.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "./app/dist"),
+        filename: "bundle.js"
+    },
+    mode: "production",
+    module: {
+        rules:[
+            {
+                test: /\\.css$/,
+                use:[MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })
+    ],
+    devtool: "source-map"
+}
