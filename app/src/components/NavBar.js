7671fab52f003ee61f804bff407896251221f04a
Douglas Freitas <douglasamf14@gmail.com>
2024-12-04 18:53:02 -0300

    Terminando estrtura da página do carrinho de compras

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -12,7 +12,7 @@ export default class NavBar extends Component {
                     <i class="bi bi-search"></i>
                 </div>
                 <ul>
-                    <li><a href="#home">Início</a></li>
+                    <li>Início</li>
                     <div class="account-buttons">
                         <li><i class="fa-regular fa-user"></i></li>
                         <li><i class="bi bi-cart"></i></li>
